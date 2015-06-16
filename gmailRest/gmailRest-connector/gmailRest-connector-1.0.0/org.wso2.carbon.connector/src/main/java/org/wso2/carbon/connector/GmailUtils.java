af14bbf12433fd29f143d128318395df882973d3
Hogwartz <nadeeshaangunasinghe@gmail.com>
2015-06-16 16:51:52 +0530

    Fixing minor issues

--- gmailRest/gmailRest-connector/gmailRest-connector-1.0.0/org.wso2.carbon.connector/src/main/java/org/wso2/carbon/connector/GmailUtils.java
+++ gmailRest/gmailRest-connector/gmailRest-connector-1.0.0/org.wso2.carbon.connector/src/main/java/org/wso2/carbon/connector/GmailUtils.java
@@ -64,7 +64,11 @@ import org.wso2.carbon.connector.core.ConnectException;
 import org.wso2.carbon.connector.core.util.ConnectorUtils;
 
 import javax.net.ssl.HttpsURLConnection;
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.DataOutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Arrays;
