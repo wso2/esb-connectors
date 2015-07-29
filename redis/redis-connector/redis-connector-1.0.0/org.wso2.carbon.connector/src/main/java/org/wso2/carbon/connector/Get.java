93506b1a5b959efcc8172053385d980f90cfc66d
naasheerali <naasheera@wso2.com>
2015-07-29 12:24:54 +0530

    Throw the syanape exception

--- redis/redis-connector/redis-connector-1.0.0/org.wso2.carbon.connector/src/main/java/org/wso2/carbon/connector/Get.java
+++ redis/redis-connector/redis-connector-1.0.0/org.wso2.carbon.connector/src/main/java/org/wso2/carbon/connector/Get.java
@@ -17,14 +17,15 @@
 package org.wso2.carbon.connector;
 
 import org.apache.synapse.MessageContext;
-import org.apache.synapse.mediators.AbstractMediator;
+import org.apache.synapse.SynapseException;
 import org.wso2.carbon.connector.core.AbstractConnector;
 import org.wso2.carbon.connector.core.ConnectException;
 import redis.clients.jedis.Jedis;
 
-public class Get extends AbstractMediator {
+public class Get extends AbstractConnector {
 
-    public boolean mediate(MessageContext messageContext) {
+    @Override
+    public void connect(MessageContext messageContext) throws ConnectException {
         try {
             Jedis jedis;
             RedisServer serverobj = new RedisServer();
@@ -38,7 +39,7 @@ public class Get extends AbstractMediator {
             }
         } catch (Exception e) {
             log.error(e);
+            throw new SynapseException("Error while connecting the server or calling the redis method",e);
         }
-        return true;
     }
 }
--- redis/redis-connector/redis-connector-1.0.0/org.wso2.carbon.connector/src/main/java/org/wso2/carbon/connector/RandomKey.java
+++ redis/redis-connector/redis-connector-1.0.0/org.wso2.carbon.connector/src/main/java/org/wso2/carbon/connector/RandomKey.java
@@ -17,12 +17,15 @@
 package org.wso2.carbon.connector;
 
 import org.apache.synapse.MessageContext;
-import org.apache.synapse.mediators.AbstractMediator;
+import org.apache.synapse.SynapseException;
+import org.wso2.carbon.connector.core.AbstractConnector;
+import org.wso2.carbon.connector.core.ConnectException;
 import redis.clients.jedis.Jedis;
 
-public class RandomKey extends AbstractMediator {
+public class RandomKey extends AbstractConnector {
 
-    public boolean mediate(MessageContext messageContext) {
+    @Override
+    public void connect(MessageContext messageContext) throws ConnectException {
         try {
             Jedis jedis;
             RedisServer serverobj = new RedisServer();
@@ -35,7 +38,7 @@ public class RandomKey extends AbstractMediator {
             }
         } catch (Exception e) {
             log.error(e);
+            throw new SynapseException("Error while connecting the server or calling the redis method",e);
         }
-        return true;
     }
 }
