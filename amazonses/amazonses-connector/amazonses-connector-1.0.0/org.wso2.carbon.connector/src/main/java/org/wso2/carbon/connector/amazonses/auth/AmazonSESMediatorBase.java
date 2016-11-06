0ffb827dfcb84d6143d4aab89767b4e55e75d533
Kanapriya <you@example.com>
2016-11-06 13:30:10 +0530

    change year in header in all files

--- amazonses/amazonses-connector/amazonses-connector-1.0.0/org.wso2.carbon.connector/src/main/java/org/wso2/carbon/connector/amazonses/auth/AmazonSESMediatorBase.java
+++ amazonses/amazonses-connector/amazonses-connector-1.0.0/org.wso2.carbon.connector/src/main/java/org/wso2/carbon/connector/amazonses/auth/AmazonSESMediatorBase.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2005-2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *  Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  *  WSO2 Inc. licenses this file to you under the Apache License,
  *  Version 2.0 (the "License"); you may not use this file except
@@ -134,7 +134,7 @@ public abstract class AmazonSESMediatorBase extends AbstractConnector {
      */
     private void buildPayload() {
         StringBuilder payload = new StringBuilder();
-        for (Map.Entry<String, String> entry : parameterMap.entrySet()) {
+        for (Entry<String, String> entry : parameterMap.entrySet()) {
 
             // Generating json Payload.
             payload.append('"');
